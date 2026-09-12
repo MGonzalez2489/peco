f8957de1add9dd5e9c29e307932b932a59af5499
ManuelGonzalez <manuel_agh@hotmail.com>
2026-09-12 15:04:45 -0600

    chore: relative paths

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,4 @@
+export * from './confirm-modal/confirm-modal.component';
+export * from './modal/modal.component';
+export * from './navbar/navbar.component';
+export * from './stat-card/stat-card.component';
