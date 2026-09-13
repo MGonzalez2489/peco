d2097e90525df0e7d88dd21c4190801d41aaa54d
ManuelGonzalez <manuel_agh@hotmail.com>
2026-09-13 12:02:57 -0600

    feat(movements): unificar la lista de movimientos en un componente compartido

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,4 +1,5 @@
 export * from './confirm-modal/confirm-modal.component';
+export * from './movement-list/movement-list.component';
 export * from './modal/modal.component';
 export * from './navbar/navbar.component';
 export * from './stat-card/stat-card.component';
---
+++ src/app/shared/components/movement-list/movement-list.types.ts
@@ -0,0 +1,6 @@
+import {Movement} from '@core/models';
+
+export interface GrupoMovimientos {
+  fecha: string;
+  movimientos: Movement[];
+}
